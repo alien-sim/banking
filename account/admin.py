22a88356fd2931f56f3160ae452d483fbab07f65
smileySim <simrajput555@gmail.com>
2020-04-02 19:03:40 +0530

    template setting and account_detail model created

--- account/admin.py
+++ account/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from account.models import User_Profile
+from account.models import User_Profile,Account_details
 # Register your models here.
 
 admin.site.register(User_Profile)
+admin.site.register(Account_details)
--- account/models.py
+++ account/models.py
@@ -22,3 +22,16 @@ class User_Profile(models.Model):
 
 	def __str__(self):
 		return self.user.id
+
+class Account_details(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	account_type = models.CharField(max_length=20)
+	date_of_joining = models.DateField()
+
+	class Meta:
+		db_table = 'account_details'
+		verbose_name_plural = 'ACCOUNT_DETAILS'
+		managed = True
+
+	def __str__(self):
+		return str(self.id)
