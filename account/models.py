1c6a1feb759a2771f6e9539794b01803d7d27c66
vishalimpinge7696 <vishalsharma.impinge@gmail.com>
2020-04-03 10:59:16 +0530

    update

--- account/models.py
+++ account/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 
+
 class User_Profile(models.Model):
 	user = models.OneToOneField(User, on_delete=models.CASCADE)
 	image = models.FileField(upload_to='profile/', null=True, blank=True)
